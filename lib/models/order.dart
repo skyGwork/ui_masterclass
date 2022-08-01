e9ecafe1247e3b0fc5b8b28b8e75c9823c9c2b0c
skyGwork <skyGworkk@gmail.com>
2022-08-01 12:46:51 +0530

    app setup

---
+++ lib/models/order.dart
@@ -0,0 +1,19 @@
+
+import 'food.dart';
+import 'restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    required this.restaurant,
+    required this.food,
+    required this.date,
+    required this.quantity,
+  });
+
+
+}
